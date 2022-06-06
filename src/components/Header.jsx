35e8e675f54b47b090b8de88858678d51b350563
braifz <braian94lp@gmail.com>
2022-06-05 21:41:12 -0300

    add component Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+    return(
+        <header className="header-title">
+            <h1> Drag the Dodecahedron! :D</h1>
+        </header>
+    )
+}
+
+export default Header;
